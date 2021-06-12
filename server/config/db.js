224692ebbbfcad2207cd8b9276d651423c57bec6
yannis94 <bengueci.yannis@gmail.com>
2021-06-12 23:17:03 +0200

    async problem fixed, now i can add and auth user with their response back

--- server/config/db.js
+++ server/config/db.js
@@ -1,69 +1,9 @@
-const { Client } = require("pg")
-
-const client = new Client({
+const conn = {
     user: 'myapp',
     host: 'localhost',
     database: 'dykm',
     password: 'thepassword',
     port: 5432,
-})
-
-/*client.query("SELECT firstname FROM users", (err, res)=> {
-    console.table(res.rows)
-})
-*/
-
-function checkUser(userData) {
-    let isUser = false
-    let query = {
-        name: 'auth-user',
-        text: 'SELECT password FROM users WHERE email = $1',
-        values: [userData.email]
-    }
-
-    client.connect()
-
-    client.query(query, (err, res) => {
-        if (err) {
-          console.log(err.stack)
-          client.end()
-        } 
-        else {
-            console.log("user : " + userData.password)
-            console.log("db : " + res.rows[0].password)
-            if (res.rows[0].password == userData.password) {
-                console.log("Auth succes")
-                client.end()
-                isUser = true
-            }
-        }
-      })
-
-    return isUser
 }
 
-function addUser(userData) {
-
-    let isAdd = false
-    let query = {
-        name: 'add-user',
-        text: 'INSERT INTO users(pseudo, firstname, lastname, email, password, created_at) VALUES($1, $2, $3, $4, $5, $6)',
-        values: [userData.pseudo, userData.firstname, userData.lastname, userData.email, userData.password, userData.created_at]
-    }
-    client.connect()
-    client
-        .query(query)
-        .then(res => {
-            
-            isAdd = true
-            client.end()
-        })
-        .catch(err => {
-            console.log(err)
-            client.end()
-        })
-
-}
-
-exports.checkUser = checkUser
-exports.addUser = addUser
+exports.conn = conn
--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const http = require("http")
 const fs = require("fs")
 const path = require("path")
+const { Client } = require("pg")
 
 const MIME = require("./config/mime")
 const DB = require("./config/db.js")
-const { json } = require("./config/mime")
 
 const PORT = process.env.PORT || 3003
 const host = "127.0.0.1"
@@ -17,22 +17,44 @@ const server = http.createServer((req, res) => {
 
     if (requestPath === "/login") {
         if (req.method === "POST") {
-            let infos = ''
+            let data = ''
             req.on("data", (chunk) => {
-                infos += chunk
+                data += chunk
             })
             req.on('end', () => {
-                console.log(JSON.parse(infos))
-                let isAuth = DB.checkUser(JSON.parse(infos))
-                console.log(isAuth)
-                if (isAuth) {
-                    res.statusCode = 200
+                let userData = JSON.parse(data)
+                let query = {
+                    name: 'auth-user',
+                    text: 'SELECT password FROM users WHERE email = $1',
+                    values: [userData.email]
                 }
-                else {
-                    console.log("non")
-                    res.statusCode = 401
-                }
-                res.end()
+                
+                const client = new Client(DB.conn)
+            
+                client.connect()
+                client
+                    .query(query)
+                    .then(response => {
+                        console.log("user : " + userData.password)
+                        console.log("db : " + response.rows[0].password)
+                        if (response.rows[0].password == userData.password) {
+                            console.log("Auth succes")
+                            client.end()
+                            res.statusCode = 200
+                        }
+                        else {
+                            console.log("Auth fail")
+                            client.end()
+                            res.statusCode = 401
+                        }
+                        res.end()
+                    })
+                    .catch(err => {
+                        console.log(err)
+                        client.end()
+                        res.statusCode = 500
+                        res.end()
+                    })
             })
             req.on('error', (err) => {
                 console.log(err)
@@ -49,12 +71,33 @@ const server = http.createServer((req, res) => {
                 infos += chunk
             })
             req.on('end', () => {
-                console.log(JSON.parse(infos))
-                //create new row in db
-                //if good > status code 200
-                //if not > status code 401
-                DB.addUser(JSON.parse(infos))
-                res.statusCode = 200
+                console.log(infos)
+
+                let userData = JSON.parse(infos)
+                let query = {
+                    name: 'add-user',
+                    text: 'INSERT INTO users(pseudo, firstname, lastname, email, password, created_at) VALUES($1, $2, $3, $4, $5, $6)',
+                    values: [userData.pseudo, userData.firstname, userData.lastname, userData.email, userData.password, userData.created_at]
+                }
+
+                const client = new Client(DB.conn)
+
+            
+                client.connect()
+
+                client
+                    .query(query)
+                    .then(response => {
+                        console.log(response)
+                        client.end()
+                        res.statusCode = 200
+                    })
+                    .catch(err => {
+                        console.log(err)
+                        client.end()
+                        res.statusCode = 500
+                    })
+                    
                 res.end()
             })
             req.on('error', (err) => {
